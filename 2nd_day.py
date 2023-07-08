090b4e85d54d5a30a85505cc12f4d9f6b5e070da
Anik Das <anikd282004@gmail.com>
2023-07-08 14:40:29 +0600

    my work

---
+++ 2nd_day.py
@@ -0,0 +1,10 @@
+a =int(input("Enter the First Number: "))
+b =int(input("Enter the Second Number: "))
+sum=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("A+B =",sum)
+print("A-B =",sub)
+print("A*B =",mul)
+print("A/B =",div)
